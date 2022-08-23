8498c8161d951103f7128e73a485b9fd45d3ee33
Mais1212 <64730510+Mais1212@users.noreply.github.com>
2022-08-23 22:33:51 +0300

    Add question module.

---
+++ tools/questions.py
@@ -0,0 +1,55 @@
+import os
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Question:
+    """Question model"""
+    question: str
+    answer: str
+
+
+def fetch_data(folder: str, file: str) -> str:
+    """Fetch data from a file."""
+    questions_file_path = os.path.join(folder, file)
+    with open(questions_file_path, "r", encoding="KOI8-R") as file:
+        data = file.read()
+    return data
+
+
+def extract_questions(file: str) -> list[Question]:
+    """Fetch questions from a file."""
+    questions = []
+    processed_questions, answers = [], []
+
+    for line in file.split("\n\n"):
+        if line.strip().startswith("Вопрос"):
+            _, question_text = line.split(sep=":", maxsplit=1)
+            processed_questions.append(
+                question_text.replace("\n", " ").strip())
+        elif line.strip().startswith("Ответ"):
+            _, answer_text = line.split(sep=":", maxsplit=1)
+            answers.append(answer_text.replace("\n", " ").strip())
+
+    for index in range(0, len(processed_questions)):
+        questions.append(
+            Question(
+                processed_questions[index],
+                answers[index]
+            )
+        )
+
+    return questions
+
+
+def main() -> None:
+    questions_folder = 'questions'
+    questions_files = os.listdir(questions_folder)
+    questions = []
+    for questions_file in questions_files:
+        file = fetch_data(questions_folder, questions_file)
+        questions.append(extract_questions(file))
+
+
+if __name__ == "__main__":
+    main()
